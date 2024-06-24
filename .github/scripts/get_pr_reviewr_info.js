7fa912c6e52c90ccd89fadabeca7624a93c244d8
dhkim <doh@whatap.io>
2024-06-24 14:02:09 +0900

    확인 부분 수정

--- .github/scripts/get_pr_reviewr_info.js
+++ .github/scripts/get_pr_reviewr_info.js
@@ -1,27 +1,25 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-// const path = require('path');
-// const fs = require('fs');
 
 function getReviewerInfo() {
   try {
     // PR 정보 가져오기
-    // const context = github.context;
-    // const pullRequest = context.payload.pull_request;
-    // const prNumber = pullRequest.number;
-    // const prTitle = pullRequest.title;
-    // const prBody = pullRequest.body;
-    // const prUrl = pullRequest.url;
-    // const userInfoFilePath= path.join(__dirname,'../json/slackUserInfo.json');
-    // fs.writeFileSync(userInfoFilePath, JSON.stringify(github));
+    const context = github.context;
+    const pullRequest = context.payload.pull_request;
+    const prNumber = pullRequest.number;
+    const prTitle = pullRequest.title;
+    const prBody = pullRequest.body;
+    const prUrl = pullRequest.url;
+
     console.log('github: ', JSON.stringify(github))
+    console.log('########## pullRequest: ', JSON.stringify(pullRequest));
     // console.log('github.context: ', JSON.stringify(github.context))
 
     // // 콘솔에 PR 정보 출력
-    // console.log(`PR 번호: ${prNumber}`);
-    // console.log(`PR 제목: ${prTitle}`);
-    // console.log(`PR 내용: ${prBody}`);
-    // console.log(`PR URL: ${prUrl}`);
+    console.log(`PR 번호: ${prNumber}`);
+    console.log(`PR 제목: ${prTitle}`);
+    console.log(`PR 내용: ${prBody}`);
+    console.log(`PR URL: ${prUrl}`);
   } catch (error) {
     core.setFailed(error.message);
   }
