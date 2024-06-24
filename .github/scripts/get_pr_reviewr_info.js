7b14aa656213a5d5acccd5b85b09254461666e54
dhkim <doh@whatap.io>
2024-06-24 12:29:45 +0900

    주석처리추가

--- .github/scripts/get_pr_reviewr_info.js
+++ .github/scripts/get_pr_reviewr_info.js
@@ -22,34 +22,34 @@ function getReviewerInfo() {
 
   return;
     // const context = github.context;
-    console.log('########## github: ', github);
-    console.log('########## context: ', context);
-    const pullRequestNumber = context.payload.pull_request.number;
-    const owner = context.repo.owner;
-    const repo = context.repo.repo;
+    // console.log('########## github: ', github);
+    // console.log('########## context: ', context);
+    // const pullRequestNumber = context.payload.pull_request.number;
+    // const owner = context.repo.owner;
+    // const repo = context.repo.repo;
   
-    const url = `https://api.github.com/repos/${owner}/${repo}/pulls/${pullRequestNumber}/reviewers`;
-    const headers = {
-      Authorization: `token ${process.env.GITHUB_TOKEN}`,
-    };
+    // const url = `https://api.github.com/repos/${owner}/${repo}/pulls/${pullRequestNumber}/reviewers`;
+    // const headers = {
+    //   Authorization: `token ${process.env.GITHUB_TOKEN}`,
+    // };
   
-    try {
-      const response = await fetch(url, { headers });
-      const reviewers = await response.json();
+    // try {
+    //   const response = await fetch(url, { headers });
+    //   const reviewers = await response.json();
   
-      const reviewerNames = [];
-      const reviewerLogins = [];
+    //   const reviewerNames = [];
+    //   const reviewerLogins = [];
   
-      for (const reviewer of reviewers) {
-        reviewerNames.push(reviewer.login);
-        reviewerLogins.push(reviewer.login);
-      }
+    //   for (const reviewer of reviewers) {
+    //     reviewerNames.push(reviewer.login);
+    //     reviewerLogins.push(reviewer.login);
+    //   }
   
-      core.setOutput('reviewerNames', reviewerNames.join(', '));
-      core.setOutput('reviewerLogin', reviewerLogins.join(', '));
-    } catch (error) {
-      core.setFailed(error.message);
-    }
+    //   core.setOutput('reviewerNames', reviewerNames.join(', '));
+    //   core.setOutput('reviewerLogin', reviewerLogins.join(', '));
+    // } catch (error) {
+    //   core.setFailed(error.message);
+    // }
   }
   
   getReviewerInfo();
