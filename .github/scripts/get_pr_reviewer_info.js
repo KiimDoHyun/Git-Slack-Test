735f07fd9f5c2b6678c1ad3eaff1755f7e563237
dhkim <doh@whatap.io>
2024-06-27 15:10:18 +0900

    수정

--- .github/scripts/get_pr_reviewer_info.js
+++ .github/scripts/get_pr_reviewer_info.js
@@ -176,13 +176,14 @@ function getReviewerInfo() {
 
       const reviewers = github.context.payload.pull_request.requested_reviewers;
       reviewers.forEach((reviewer) => {
+        const reviewerInfo = slackUserInfo[reviewer.login];
         console.log('########## reviewer: ', reviewer);
-        const channelId = slackUserInfo[reviewer.login].directMessageId;
+        console.log('########## reviewerInfo: ', reviewerInfo);
+        const channelId = reviewerInfo? reviewerInfo.directMessageId : slackUserInfo['KiimDoHyun'.directMessageId];
 
-        sendSlackMessage({blocks, channelId})
+        // sendSlackMessage({blocks, channelId})
       })
 
-      console.log('context.payload.pull_request', context.payload.pull_request);
       // blocks.push({
       //   "type": "context",
       //   "elements": [
@@ -228,10 +229,7 @@ function getReviewerInfo() {
       prTitle = context.payload.pull_request.title;
       link = context.payload.pull_request.html_url
     } else if (context.eventName === 'pull_request_review'){
-      console.log('context.payload.review.user', context.payload.review.user)
-      type = '코드리뷰'
       if(context.payload.action === 'submitted') {
-        type += '추가'
         body = context.payload.review.body
 
         console.log('########## context.payload.pull_request.user: ', context.payload.pull_request.user);
@@ -261,70 +259,6 @@ function getReviewerInfo() {
       // context.payload.pull_request 에서 pr 정보 추출
       prTitle = context.payload.pull_request.title;
     }
-
-    console.log('########## context: ', context);
-    // console.log('########## context.payload.issue: ', context.payload['issue']);
-    // console.log('########## context.payload.pull_request: ', context.payload['pull_request']);
-
-    const messageId = slackUserInfo['KiimDoHyun'];
-
-    // reviewers.forEach((reviewer) => {
-    //   const messageId = slackUserInfo['KiimDoHyun'];
-    //   // const messageId = slackUserInfo[reviewer.login || 'areumsheep'];
-
-    //   console.log('#### messageId: ', messageId);
-    //   fetch(`https://slack.com/api/chat.postMessage`, {
-    //     method: "POST",
-    //         headers: {
-    //             'Content-Type': 'application/json; charset=utf-8',
-    //             Authorization: `Bearer ${accessToken}` // 헤더에 Bearer 토큰 추가
-    //           },
-    //           body: JSON.stringify({
-    //             channel: messageId,
-    //             text: `트리거된 액션 정보:${context.payload.action}`
-    //           })
-    //   }).then(async(res) => {
-    //     const response = await res.json()
-    //     console.log('res', response)}).catch((e) => {
-    //     console.log('실패', e)
-    //   })
-    // })
-
-    // pr 리뷰어로 선정되었을 때 메세지 전달
-    // D078A6G7405 -> 내꺼 개인 채널 아이디임
-    // [Notice] 리뷰어로 등록되었습니다.
-    // - 제목:
-    // - 내용: 
-    // - 작성자: 
-    // - 라벨:
-    // - pr 주소: 
-
-
-
-
-    // 무조건 배열로 들어있음
-    // github.context.payload.pull_request.requested_reviewers;
-
-    // 반복문으로 날려야 한다.
-    // 실패가 있으면 에러 메세지를 나한테 
-    /*
-    const reviweres = getReviewers();
-
-    for
-    */
-    
-    // console.log('github: ', JSON.stringify(github))
-    // console.log('########## pullRequest: ', JSON.stringify(pullRequest));
-    // // console.log('github.context: ', JSON.stringify(github.context))
-
-    // console.log('트리거된 액션 종류 1', context.payload.action);
-    // console.log('트리거된 액션 종류 2', context.payload.action);
-    // // // 콘솔에 PR 정보 출력
-    // console.log(`PR 번호: ${prNumber}`);
-    // console.log(`PR 제목: ${prTitle}`);
-    // console.log(`PR 내용: ${prBody}`);
-    // console.log(`PR URL: ${prUrl}`);
-    // console.log('리뷰어 정보', JSON.stringify(github.context.payload.pull_request.requested_reviewers));
   } catch (error) {
     core.setFailed(error.message);
   }
